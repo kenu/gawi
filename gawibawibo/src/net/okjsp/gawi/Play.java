faa1ee87a155ce087139635243c4b093061f6263
kenu <kenu@okjsp.pe.kr>
2012-03-03 16:57:31 +0900

    String array

--- gawibawibo/src/net/okjsp/gawi/Play.java
+++ gawibawibo/src/net/okjsp/gawi/Play.java
@@ -6,12 +6,10 @@ public class Play {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		String item1 = "가위";
-		System.out.println(item1);
-		String item2 = "바위";
-		System.out.println(item2);
-		String item3 = "보";
-		System.out.println(item3);
+		String[] items = {"가위", "바위", "보"};
+		System.out.println(items[0]);
+		System.out.println(items[1]);
+		System.out.println(items[2]);
 	}
 
 }
