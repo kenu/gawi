d7ef44a1c2889819bd8bca511171a2da8e54cbc3
kenu <kenu@okjsp.pe.kr>
2012-03-03 16:53:53 +0900

    가위바위보 변수

---
+++ gawibawibo/src/net/okjsp/gawi/Play.java
@@ -0,0 +1,17 @@
+package net.okjsp.gawi;
+
+public class Play {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		String item1 = "가위";
+		System.out.println(item1);
+		String item2 = "바위";
+		System.out.println(item2);
+		String item3 = "보";
+		System.out.println(item3);
+	}
+
+}
