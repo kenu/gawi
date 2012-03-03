e2ac3e06a9d52ac9107495d40a1ac31268013352
kenu <kenu@okjsp.pe.kr>
2012-03-03 18:05:12 +0900

    게임 반복

--- gawibawibo/src/net/okjsp/gawi/Play.java
+++ gawibawibo/src/net/okjsp/gawi/Play.java
@@ -9,41 +9,47 @@ public class Play {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		String[] items = {"가위", "바위", "보"};
-		for (int i = 0; i < items.length; i++) {
-			System.out.println(i + ":" +items[i]);
+		while (game()) {
+			System.out.println("====");
 		}
-		
+	}
+
+	public static boolean game() {
+		String[] items = { "가위", "바위", "보" };
+		for (int i = 0; i < items.length; i++) {
+			System.out.println(i + ":" + items[i]);
+		}
+
 		System.out.print("----\n선택하세요:");
-		
+
 		Scanner scanner = new Scanner(System.in);
 		String next = scanner.next();
 		int choice = Integer.parseInt(next);
-		
+
 		if (choice < 0 || choice > 2) {
-			System.out.println("0~2 숫자만 입력해주세요.\n");
-			return;
+			System.out.println("0~2 숫자만 입력해주세요.\n게임을 종료합니다.");
+			return false;
 		}
 
 		int computerChoice = new Random(System.nanoTime()).nextInt(3);
 
-		System.out.println("----\n컴퓨터: " +
-				items[computerChoice] +
-				"\n당신: " +
-				items[choice]);
-		
+		System.out.println("----\n" + "당신: " + items[choice] + "컴퓨터: "
+				+ items[computerChoice] + "\n");
+
 		String judgement = judge(choice, computerChoice);
-		
+
 		System.out.println("----\n" + judgement);
+
+		return true;
 	}
 
 	public static String judge(int choice, int computerChoice) {
 		String judgement;
-		
+
 		if (choice == computerChoice) {
 			judgement = "비겼습니다.";
-		} else if ((choice == 0 && computerChoice == 1) 
-				|| (choice == 1 && computerChoice == 2) 
+		} else if ((choice == 0 && computerChoice == 1)
+				|| (choice == 1 && computerChoice == 2)
 				|| (choice == 2 && computerChoice == 0)) {
 			judgement = "컴퓨터가 이겼습니다.";
 		} else {
