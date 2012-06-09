3e5e422bc2d69909483103110b9ffd18a570065a
kenu <kenu.heo@gmail.com>
2012-06-09 16:40:18 +0900

    add ddl

--- gawibawibo/gamequery.sql
+++ gawibawibo/gamequery.sql
@@ -1,5 +1,11 @@
 select * from game;
 
+		
+	
+GRANT ALL PRIVILEGES ON *.* TO javauser@localhost
+   IDENTIFIED BY 'javadude' WITH GRANT OPTION;
+create database javatest;
+use javatest;
 
 /*Table structure for table `game` */
 drop table game;
@@ -18,11 +24,3 @@ insert into `game` values (1,1,1,'비겼습니다.','2012-03-06 06:49:57'),(2,2,
 (3,0,1,'컴퓨터가 이겼습니다.','2012-03-06 06:52:34'),(4,1,2,'컴퓨터가 이겼습니다.','2012-03-06 08:19:26'),
 (5,0,1,'컴퓨터가 이겼습니다.','2012-03-06 08:20:06'),(6,1,0,'당신이 이겼습니다.','2012-03-06 08:20:13');
 
-/*Table structure for table `simple_information` */
-
-CREATE TABLE `simple_information` (
-  `info_id` int(11) DEFAULT NULL,
-  `info_content` varchar(50) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-/*Data for the table `simple_information` */
